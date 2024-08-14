d050510d6b8d115c73ae35bc5534debbc075bac8
M8Iron <gaspard.maxime90@gmail.com>
2024-08-14 15:23:26 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-Token = "TOKEN"
+Token = "https://discord.com/api/webhooks/1273270103324102656/rD0G31fXxdBHapC-LaSom1yEE_hDl7zPmaDWAPK1dlnvJbgf86xSOrGcCfhrXuzAmqys"
 Prefix = '.'
 PointsName = "Goon Tokens"
 Log_channel = 821442828056002602 # Channel where all shop purchases are logged.
-POINTS_TO_BE_GIVEN_PER_MESSAGE = 2 # Points which will be given to members per message.
+POINTS_TO_BE_GIVEN_PER_MESSAGE = 0 # Points which will be given to members per message.
