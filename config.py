41f42bf010ba812e48ae41e829a2743267054506
awezhxm <bernardinozham@gmail.com>
2025-02-28 20:18:44 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-Token = "TOKEN"
+Token = "MTM0Mzg4NDY5NDgzNTY5MTU3MA.G_2rJB.5fD8AHAPzd8khO3hRohHGgMO29f5an9vognPt4"
 Prefix = '.'
-PointsName = "Goon Tokens"
+PointsName = "awezh"
 Log_channel = 821442828056002602 # Channel where all shop purchases are logged.
 POINTS_TO_BE_GIVEN_PER_MESSAGE = 2 # Points which will be given to members per message.
